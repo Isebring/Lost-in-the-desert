bcc727b906c97d8674dedf52b408f823fc2d96c1
Hampus Isebring <hampus.isebring@gmail.com>
2022-11-17 09:24:49 +0100

    Variabler för inputs, berättartext och rubrik

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 window.addEventListener('DOMContentLoaded', clearInput)
 
+/** Variables for the story */
+
+let textInput = document.getElementById("user_inp");
+let displayInp = document.getElementById("displayInput");
+let nar = document.getElementById("narrator");
+let gameStarted = false;
+let heading = document.getElementById('adventure');
+
+textInput.addEventListener('input', showInp);
+
+
+
 function clearInput(){
   let getValue= document.getElementById("user_inp");
     if (getValue.value !="") {
