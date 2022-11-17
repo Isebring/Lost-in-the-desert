a5b86e755956ba9b8ddebf3aa27e82311b6d6bb1
Hampus Isebring <hampus.isebring@gmail.com>
2022-11-17 12:12:12 +0100

    Tog bort div som inte längre behövs

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ document.title = "Lost in the desert";
 /** Variables for the story */
 
 let textInput = document.getElementById("user_inp");
-let displayInp = document.getElementById("displayInput");
 let nar = document.getElementById("narrator");
 let gameStarted = false;
 let heading = document.getElementById('adventure');
